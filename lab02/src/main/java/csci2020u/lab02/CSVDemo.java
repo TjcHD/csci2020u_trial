bc0b01b3c89fab7b56eb7f832ca24ac3b90d5d66
TjcHD <35234169+TjcHD@users.noreply.github.com>
2018-01-29 20:33:08 -0500

    Update CSVDemo.java

--- lab02/src/main/java/csci2020u/lab02/CSVDemo.java
+++ lab02/src/main/java/csci2020u/lab02/CSVDemo.java
@@ -13,7 +13,8 @@ CSVFormat.DEFAULT.withHeader("First Name", "Last Name", "SID",
 			String lastName = record.get("Last Name");
 			String firstName = record.get("First Name");
 			String grade = record.get("Grade");
-			System.out.println(firstName + " " + lastName+ "("+ grade + ")");}
+			System.out.println(firstName + " " + lastName+ "("+ grade + ")");
+		}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
